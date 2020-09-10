a18413ef00e7c7b3c3ecc154dabf89e1eef8684e
Eli Huebner <elimhuebner@gmail.com>
2020-09-10 14:03:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@
 def prime?(integer)
   if integer < 1
     false
-  end
-  factor_range = (2..Math.sqrt(integer))
-  factor_array = factor_range.to_a
-  factor_array.each do 
+  else
+    factor_range = (2..Math.sqrt(integer))
+    factor_array = factor_range.to_a
+    factor_array.each do 
 end

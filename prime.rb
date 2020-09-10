92d0233885672171bf8553c62a0f0d3a425d1162
Eli Huebner <elimhuebner@gmail.com>
2020-09-10 14:10:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ require 'pry'
 def prime?(integer)
   if integer < 1
     false
-  elsif integer == 2
+  elsif integer == 1 || integer == 2 || integer == 3
     true
   else
     factor_range = (2..Math.sqrt(integer))

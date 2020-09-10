1f8dc298b98b3c67ae0a5fcb10ecac65497f478d
Eli Huebner <elimhuebner@gmail.com>
2020-09-10 13:55:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(integer)
-  factor_range = (2..Integer.sqrt(integer))
+  factor_range = (2..Math.sqrt(integer))
   factor_array = factor_range.to_a
   factor_array.each do |factor|
     if integer % factor = 0

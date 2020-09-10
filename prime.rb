9322be8c53caed2205d50e3fde68ab0a0bfdd5c6
Eli Huebner <elimhuebner@gmail.com>
2020-09-10 14:10:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,8 @@ require 'pry'
 def prime?(integer)
   if integer < 1
     false
+  elsif integer == 2
+    true
   else
     factor_range = (2..Math.sqrt(integer))
     factor_array = factor_range.to_a

077d143bff90aaab4743b1230ca735899ba46961
Eli Huebner <elimhuebner@gmail.com>
2020-09-10 14:34:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
+require 'pry'
+
 # Add  code here!
+def prime?(integer)
+  if integer <= 1
+    false
+  elsif integer == 2 || integer == 3
+    true
+  else
+    test_range = (2..Math.sqrt(integer))
+    test_array = test_range.to_a
+    factor_array = [1]
+    if test_array.find { |possible_factor| integer % possible_factor == 0}
+      false
+    else
+      true
+    end
+  end
+end
